0398178560482cadf5ab0f3726177de3d55a1c06
Sam Parkinson <parkinsonsp42@gmail.com>
2024-11-29 20:23:22 -0500

    Commit from correct folder to include all relevant changes

---
+++ test/receipts-endpoints.spec.js
@@ -0,0 +1,5 @@
+const app = require("../src/app");
+
+describe('Receipts Endpoints', function() {
+
+});
---
+++ test/setup.js
@@ -0,0 +1,5 @@
+import { expect } from "chai";
+import supertest from "supertest";
+
+global.expect = expect;
+global.supertest = supertest;
