9a4979c99ff479a3c4bd18893368bc2381ddc544
Sam Parkinson <parkinsonsp42@gmail.com>
2024-11-29 20:22:51 -0500

    Create server and abstract app, prepare for testing

--- src/app.js
+++ src/app.js
@@ -12,6 +12,4 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
-app.listen (8000, () => {
-  console.log(`Server listening at http://localhost:8000`);
-})
+export default app;
